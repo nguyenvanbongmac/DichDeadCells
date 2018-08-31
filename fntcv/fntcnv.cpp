b2aafdc4a3c25fb6f4252547d5339f3464aa5327
VanKaiser <quyenvanboqmak@gmail.com>
2018-08-31 10:18:31 +0700

    update

--- fntcv/fntcnv.cpp
+++ fntcv/fntcnv.cpp
@@ -1,5 +1,28 @@
 #include "ham.h"
+#include <fstream>
 
 int main(int argc, char* argv[]){
+	fstream infile(argv[1], std::ios::in);
+	string out = argv[1] + ".txt";
+	fstream outfile(out, std::ios::out);
+	std::tring x;
+	getline(infile, x);
+	x.erase(x.begin(), x.find('\n')+1);
+	getline(infile, x);
+	x.erase(x.begin(), x.find('\n')+1);
+	getline(infile, x);
+	x.erase(x.begin(), x.find('\n')+1);
+	getline(infile, x);
+	int i = 1, allchar = findchar(x);
+	x.erase(x.begin(), x.find('\n')+1);
 	
+	do
+	{
+		getline(infile,x);
+		outfile << id(x) << xadvandce(x) << xoffset(x) << yoffset(x) << tdx(x) << tdy(x) << width(x) << height(x) << "00";
+		x.erase(x.begin(), x.find('\n')+1);
+		i++;
+	} while(i<allchar);
+	infile.close();
+	outfile.close();
 }
--- fntcv/ham.h
+++ fntcv/ham.h
@@ -1,3 +1,5 @@
+#include <sstream>
+
 void findAndReplaceAll(std::string & data, std::string toSearch, std::string replaceStr)
 {
 	// Get the first occurrence
@@ -20,6 +22,15 @@ std::string hoandoi(std::string x){
 	s2 = s3;
 	return (s1 + s2);
 }
+int findchar(std::string x){
+	size_t i = x.find('');
+	std::string char = x.substr(i+);
+	std::stringstream tonum;
+	tonum << char;
+	int num;
+	tonum >> num;
+	return num;
+}
 std::string id(std::string x){
 	int k;
 	std::string s, sh;
@@ -30,32 +41,92 @@ std::string id(std::string x){
 	ss >> k;
 	ssh << setfill('0') << setw(4) << std::hex << k;
 	ssh >> sh;
-	return sh;
+	return hoandoi(sh);
 	
 }
 std::string tdx(std::string x){
 	int k;
 	std::string s, sh;
 	std::stringstream ss, ssh;
-	size_t i = x.find('id=');
-	s = x.substr(i+3, 4);
+	size_t i = x.find('x=');
+	s = x.substr(i+2, 4);
 	ss << s;
 	ss >> k;
 	ssh << setfill('0') << setw(4) << std::hex << k;
 	ssh >> sh;
-	return sh;
+	return hoandoi(sh);
 	
 }
 std::string tdy(std::string x){
 	int k;
 	std::string s, sh;
 	std::stringstream ss, ssh;
-	size_t i = x.find('id=');
-	s = x.substr(i+3, 4);
+	size_t i = x.find('y=');
+	s = x.substr(i+2, 4);
 	ss << s;
 	ss >> k;
 	ssh << setfill('0') << setw(4) << std::hex << k;
 	ssh >> sh;
-	return sh;
+	return hoandoi(sh);
 	
+}
+std::string width(std::string x){
+	int k;
+	std::string s, sh;
+	std::stringstream ss, ssh;
+	size_t i = x.find('width=');
+	s = x.substr(i+6, 4);
+	ss << s;
+	ss >> k;
+	ssh << setfill('0') << setw(2) << std::hex << k;
+	ssh >> sh;
+	return sh;
+}
+std::string height(std::string x){
+	int k;
+	std::string s, sh;
+	std::stringstream ss, ssh;
+	size_t i = x.find('height=');
+	s = x.substr(i+7, 4);
+	ss << s;
+	ss >> k;
+	ssh << setfill('0') << setw(2) << std::hex << k;
+	ssh >> sh;
+	return sh;
+}
+std::string xoffset(std::string x){
+	int k;
+	std::string s, sh;
+	std::stringstream ss, ssh;
+	size_t i = x.find('xoffset=');
+	s = x.substr(i+8, 4);
+	ss << s;
+	ss >> k;
+	ssh << setfill('0') << setw(2) << std::hex << k;
+	ssh >> sh;
+	return sh;
+}
+std::string yoffset(std::string x){
+	int k;
+	std::string s, sh;
+	std::stringstream ss, ssh;
+	size_t i = x.find('yoffset=');
+	s = x.substr(i+6, 4);
+	ss << s;
+	ss >> k;
+	ssh << setfill('0') << setw(2) << std::hex << k;
+	ssh >> sh;
+	return sh;
+}
+std::string xadvandce(std::string x){
+	int k;
+	std::string s, sh;
+	std::stringstream ss, ssh;
+	size_t i = x.find('xadvandce=');
+	s = x.substr(i+10, 4);
+	ss << s;
+	ss >> k;
+	ssh << setfill('0') << setw(2) << std::hex << k;
+	ssh >> sh;
+	return sh;
 }
